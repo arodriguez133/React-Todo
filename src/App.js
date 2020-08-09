f233ed7592129e2551d6dab5dc52c151a598f99b
Adan Rodriguez <adanrodriguez@Kourtneys-MBP.hitronhub.home>
2020-08-09 17:39:29 -0500

    imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
+import './components/TodoComponents/Todo.css';
 
 const todosData = [
   {
